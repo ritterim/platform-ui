fa7c83535921e5d4f1a4babd4bcb69d239a2b4b0
Ted Krueger <ted.krueger@ritterim.com>
2019-08-08 15:35:26 -0400

    dropdown menus styles full height

--- src/assets/js/src/mobile-menu.js
+++ src/assets/js/src/mobile-menu.js
@@ -7,3 +7,20 @@ if (menuAction) {
     body.classList.toggle('menu-open');
   });
 }
+
+var hasDropdown = document.querySelector('.has-dropdown');
+
+if (hasDropdown) {
+
+  function handleDropDownClick() {
+    [].forEach.call(document.querySelectorAll('.has-dropdown'), function(el) {
+      el.addEventListener('click', function() {
+        this.classList.toggle('active');
+      })
+    });
+  }
+
+  if (window.innerWidth < 768) {
+    handleDropDownClick();
+  }  
+}
