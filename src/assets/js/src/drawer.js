69c97a11c60f09f76133b0b97a3c3dc4b6534c79
Ted Krueger <ted.krueger@ritterim.com>
2019-08-20 08:25:07 -0400

    clean up code structure, refactor icon menu

--- src/assets/js/src/drawer.js
+++ src/assets/js/src/drawer.js
@@ -24,3 +24,64 @@ if(openDrawer) {
     });
   });
 }
+
+const drawerOpen = document.querySelectorAll('.js-open-right-drawer');
+const drawerClose = document.querySelectorAll('.js-drawer-right-close');
+const rightDrawer = document.querySelector('.js-drawer-right');
+const sideContent = document.querySelector('.js-side-content');
+const showContent = document.querySelectorAll('.js-show-content');
+
+if(rightDrawer) {
+  drawerOpen.forEach(open => {
+    open.addEventListener('click', openDrawerRight);
+  });
+  
+  drawerClose.forEach(close => {
+    close.addEventListener('click', closeDrawerRight);
+  })
+  
+  function openDrawerRight() {
+    const target = this;
+    const targetOpen = target.getAttribute('data-open');
+  
+    rightDrawer.classList.add('slide-right-enter-active');
+    rightDrawer.classList.remove('slide-right-leave-active');
+  
+    // hide all drawer content
+    rightDrawer
+      .querySelectorAll('[data-inner]')
+      .forEach(inner => inner.setAttribute('hidden', true));
+  
+    // show selected content
+    rightDrawer
+      .querySelector('[data-inner="'+targetOpen+'"]')
+      .removeAttribute('hidden');
+  }
+  
+  function closeDrawerRight() {
+    rightDrawer.classList.add('slide-right-leave-active');
+    rightDrawer.classList.remove('slide-right-enter-active');
+  }
+  
+  showContent.forEach(show => {
+    show.addEventListener('click', showSideContent);
+  })
+  
+  function showSideContent() {
+    const target = this;
+    const targetOpen = target.getAttribute('data-open');
+  
+  
+    // hide all drawer content
+    sideContent
+      .querySelectorAll('[data-content-inner]')
+      .forEach(inner => inner.setAttribute('hidden', true));
+  
+    // show selected content
+    sideContent
+      .querySelector('[data-content-inner="'+targetOpen+'"]')
+      .removeAttribute('hidden');
+  
+    closeDrawerRight();
+  }
+}
