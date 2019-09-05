1688b3c90ee676c02cdb6898a04091d07c435b29
Ted Krueger <ted.krueger@ritterim.com>
2019-09-05 10:26:03 -0400

    accordion changes and border right on icon menu

---
+++ src/assets/js/src/accordion.js
@@ -0,0 +1,10 @@
+let accordionHeader = document.querySelectorAll('.rim-accordion__header');
+
+if (accordionHeader) {
+  accordionHeader.forEach(function(el) {
+    el.addEventListener('click', function() {
+      let accordion = el.parentElement;
+      accordion.classList.toggle('active');
+    });
+  });
+}
