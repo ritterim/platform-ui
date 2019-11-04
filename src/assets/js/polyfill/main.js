72cc10ad86af365102cec8b32bb06ae0cf346038
Andrew Rady <andrew.rady@ritterim.com>
2019-11-04 09:38:28 -0700

    updates logic for HTMLCollections

--- src/assets/js/polyfill/main.js
+++ src/assets/js/polyfill/main.js
@@ -1,4 +1,6 @@
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
+}
+if(window.HTMLCollection && !HTMLCollection.prototype.forEach) {
   HTMLCollection.prototype.forEach = Array.prototype.forEach;
 }
