d921f41f8777f35d869555187b1bf33c83b7d0f0
Ted Krueger <ted.krueger@ritterim.com>
2019-11-18 14:23:40 -0500

    mobile and desktop set

--- src/assets/js/src/tabs.js
+++ src/assets/js/src/tabs.js
@@ -11,26 +11,29 @@ if (tabList) {
     tab.addEventListener('click', changeTabs);
   });
 
-  if (window.location.hash) {
-    tabs.forEach(tab => {
-      if (window.location.hash === tab.hash) {
-        tabList.children[0].firstElementChild.setAttribute('aria-selected', false);
-        tab.setAttribute('aria-selected', true);
-      }
-    });
-    tabPanel.forEach(panel => {
-      if (window.location.hash.replace('#', '') === panel.id) {
-        tabPanel[0].setAttribute('hidden', true);
-        panel.removeAttribute('hidden');
-      }
-    });
-  } else {
-    console.log('no hash');
-    setFirstTab();
+  function checkHash() {
+    if (window.location.hash) {
+      tabs.forEach(tab => {
+        if (window.location.hash === tab.hash) {
+          tabList.children[0].firstElementChild.setAttribute('aria-selected', false);
+          tab.setAttribute('aria-selected', true);
+        }
+      });
+      tabPanel.forEach(panel => {
+        if (window.location.hash.replace('#', '') === panel.id) {
+          tabPanel[0].setAttribute('hidden', true);
+          panel.removeAttribute('hidden');
+          panel.classList.add('tab-active');
+        }
+      });
+    } else {
+      setFirstTab();
+    }
   }
 
+  checkHash();
+
   function setFirstTab() {
-    console.log('test');
     tabList.children[0].firstElementChild.setAttribute('aria-selected', true);
     tabPanel[0].removeAttribute('hidden');
   };
@@ -104,26 +107,44 @@ if (tabList) {
   }
 
   function resetTabs() {
-    console.log('tabs reset');
     tabs.forEach(node => {
       node.setAttribute('aria-selected', false);
     });
   }
 
-  function mobileTabs(e) {    
+  function removeLocationHash(){
+    var noHashURL = window.location.href.replace(/#.*$/, '');
+    window.history.replaceState('', document.title, noHashURL) 
+  }
+
+  function mobileTabs(e) {
+    let id = e.target.parentNode.id;
 
     // Allow clicks inside content
     if (e.target.closest('.pui-tab-panel__inner')) return;
     
     if (e.currentTarget.classList.contains('tab-active')) {
+      // close clicked panel
       removeActiveClass();
+      removeLocationHash();
     } else {
+      // open clicked panel
+      setActiveTab(id);
       removeActiveClass();
       e.currentTarget.classList.add('tab-active');
       e.currentTarget.removeAttribute('hidden');
     }    
   }
 
+  function setActiveTab(id) {
+    resetTabs();
+    let newTab =  document.querySelector(`a[href="#${id}"]`);
+    
+    newTab.setAttribute('aria-selected', true);
+    window.location.hash = `${id}`;
+  }
+
+  // mobile resolution
   if(width < 768) {
     resetTabs();
     tabPanel.forEach(panel => {
@@ -159,18 +180,5 @@ if (tabList) {
     if(width < 768) {
       window.addEventListener('resize', handleResize);
     }
-     
-   
-
-    // if(width > 768 && width < 769) {
-    //   console.log(width);
-    //   tabPanel.forEach(panel => {
-    //     panel.addEventListener('click', mobileTabs);
-    //   });
-    // } else {      
-    //   console.log('else if');
-    //   resetTabs();
-    //   setFirstTab();
-    // }
   };
 }
