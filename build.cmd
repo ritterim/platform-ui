2038534da42edbb23507de00315b933496d12fdc
Alex Oragwu <alexoragz@users.noreply.github.com>
2019-12-03 13:45:46 -0500

    Default pr number to zero

--- build.cmd
+++ build.cmd
@@ -3,15 +3,17 @@ pushd %~dp0
 setlocal
 set pull_request_number=%APPVEYOR_PULL_REQUEST_NUMBER%
 
+if "%pull_request_number%"=="" (
+  set pull_request_number=0
+)
+
 :Build
 call npm install
 if %ERRORLEVEL% neq 0 goto BuildFail
 
 call npm run build
-echo "Error level: %ERRORLEVEL%"
 if %ERRORLEVEL% neq 0 goto BuildFail
 
-echo "pull_request_number: %pull_request_number%"
 if %pull_request_number% gtr 0 (
   echo "In pull request. Not Deploying."
   CALL npm run browser-visual-tests
