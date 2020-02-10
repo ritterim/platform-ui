191bcdc7ef4375f5757339756e734a3fa1d0c92c
Ted Krueger <ted.krueger@ritterim.com>
2020-02-10 11:51:13 -0500

    keyboard accessibility on menu

--- src/assets/js/src/menu.js
+++ src/assets/js/src/menu.js
@@ -3,16 +3,28 @@ let  hasDropdown = document.querySelectorAll('.has-dropdown');
 
 if (dropdownMenu) {
   dropdownMenu.forEach(menu => {
-    menu.classList.add('hide-mobile-up')
+    menu.classList.add('hidden')
   });
 }
+
+let openDropdown = (item) => {
+  Array.from(item.childNodes).find(x => {
+    if(x.classList && Array.from(x.classList).includes('dropdown-menu'))
+      x.classList.toggle('hidden');
+  })
+}
+
 if (hasDropdown) {
   hasDropdown.forEach(menu => {
-    menu.addEventListener('click', () => {
-      Array.from(menu.childNodes).find(x => {
-        if(x.classList && Array.from(x.classList).includes('dropdown-menu'))
-          x.classList.toggle('hide-mobile-up');
-      })
+    menu.addEventListener('click', (e) => {
+      e.preventDefault();
+      openDropdown(menu);
+    })
+    menu.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13) {
+        e.preventDefault();
+        openDropdown(menu);
+      }      
     })
   });
 }
