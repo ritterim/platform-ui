070228dfd1ea973dcd6408e02fcb1d05fa64483a
Ted Krueger <ted.krueger@ritterim.com>
2020-02-12 13:12:55 -0500

    remove tab specific focus styles on pui radios and checks

--- src/assets/js/src/accordion.js
+++ src/assets/js/src/accordion.js
@@ -17,37 +17,3 @@ if (accordionHeader) {
   });
 }
 
-let inputCheck = document.querySelectorAll('input[type=checkbox]');
-let noFocus = document.querySelectorAll('.no-focus'); 
-
-if(inputCheck) {
-  console.log(inputCheck.length);
-  inputCheck.forEach(function(el){     
-    el.addEventListener('click', function() {
-    console.log(noFocus.length);
-      if(el.classList.contains('no-focus')) {
-        el.classList.remove('no-focus');
-        el.classList.add('no-focus');
-      } else {
-        el.classList.add('no-focus');
-      }
-      if(el.checked) {
-        console.log('checked');
-      } else {
-        console.log('not checked');
-      }
-    })
-    // el.addEventListener('keydown', function(e){
-    //   if (e.keyCode === 9) {
-    //     if (el === document.activeElement) {
-    //       console.log('tabbed')
-    //       el.classList.add('focus');
-    //     } else {
-    //       console.log('not tabbed')
-    //       el.classList.remove('focus');
-    //     }        
-    //   }
-    // })
-  })
-}
-
