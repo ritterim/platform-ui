2733a90a39587f9e1272a158df568d4be2336da1
Ted Krueger <ted.krueger@ritterim.com>
2020-12-15 09:26:28 -0500

    remove --left specificity from the info box JS

--- src/assets/js/src/info-box.js
+++ src/assets/js/src/info-box.js
@@ -1,15 +1,12 @@
-let infoBox = document.querySelector('.pui-info-box, .info-box');
-let infoBox2 = document.querySelector('.pui-info-box--left, .info-box--left');
+let infoBox = document.querySelectorAll('.pui-info-box, .info-box');
 
-if(infoBox && infoBox2) {
-  let hidden = document.querySelector('.toggle-hidden');
-  hidden.addEventListener('click', button => {
-    hidden.nextSibling.nextSibling.classList.toggle('hidden');
+if(infoBox) {
+  let hidden = document.querySelectorAll('.toggle-hidden');
+  hidden.forEach(button => {
+    button.addEventListener('click', e => {
+      e.preventDefault();
+      button.nextSibling.nextSibling.classList.toggle('hidden');
+    });
   });
-
-  let hiddenLeft = document.querySelector('.toggle-hidden--left');
-  hiddenLeft.addEventListener('click', e => {
-    e.preventDefault();
-    hiddenLeft.nextSibling.nextSibling.classList.toggle('hidden');
-  });
+  
 }
