287708b2ff30bdb1e4089af2ae9b81be17c2295e
Ted Krueger <ted.krueger@ritterim.com>
2020-12-16 11:06:46 -0500

    close info box JS

--- src/assets/js/src/info-box.js
+++ src/assets/js/src/info-box.js
@@ -2,11 +2,21 @@ let infoBox = document.querySelectorAll('.pui-info-box, .info-box');
 
 if(infoBox) {
   let hidden = document.querySelectorAll('.toggle-hidden');
+  let boxClose = document.querySelectorAll('.info-box__close');
+  
   hidden.forEach(button => {
     button.addEventListener('click', e => {
       e.preventDefault();
       button.nextSibling.nextSibling.classList.toggle('hidden');
     });
   });
-  
+
+  if (boxClose) {
+    boxClose.forEach(btn => {
+      btn.addEventListener('click', e => {
+        e.preventDefault();
+        btn.parentElement.classList.toggle('hidden');
+      });
+    });
+  }
 }
