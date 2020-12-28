b9b2ceca192ad759bdd05d44d16183b7e7e4e230
Ted Krueger <ted.krueger@ritterim.com>
2020-12-28 16:29:33 -0500

    JS responsive working

--- src/assets/js/src/tabs.js
+++ src/assets/js/src/tabs.js
@@ -1,68 +1,95 @@
 const tabList = document.querySelector('.pui-tabs, .tabs');
 
 if (tabList) {
-  const tabs = document.querySelectorAll('.pui-tab, .tab');
-  const pageWidth = window.innerWidth;
+  const tabs = document.querySelectorAll('.pui-tab, .tab');  
 
   let mobileTabs = (tab) => {
+    if(!tab.previousElementSibling) debugger;
     if(tab.previousElementSibling.checked) {
       if(tab.previousElementSibling.classList.contains('un-checked')) {
         tab.previousElementSibling.classList.remove('un-checked');
         tab.previousElementSibling.setAttribute('checked', true);
+        console.log('mobiletabs if');
       } else {
         uncheck(tab);
       }
     } else {
       clearUnChecked();
+      console.log('mobiletabs else');
       tab.previousElementSibling.setAttribute('checked', true);
       return;
     }
   };
 
   let clearUnChecked = () => {
-      tabs.forEach(tab =>  {
-        const checked = tab.closest('.pui-tabs, .tabs');
-        checked.querySelectorAll('input[checked="true"]').forEach(check => {
-          check.setAttribute('checked', false);
-        })
-
-        // removes un-checked from all tabs
-        if(tab.previousElementSibling.classList.contains('un-checked')) {
-          tab.previousElementSibling.classList.remove('un-checked');
-          tab.previousElementSibling.removeAttribute('checked');
-        }
+    tabs.forEach(tab =>  {
+      const checked = tab.closest('.pui-tabs, .tabs');
+      checked.querySelectorAll('input[checked="true"]').forEach(check => {
+        check.setAttribute('checked', false);
       })
-    };
 
-    // sets checked attribute to false
-    // adds class to hide pui-tab-panel with css
-    let uncheck = (tab) => {
-      tab.previousElementSibling.setAttribute('checked', false);
-      tab.previousElementSibling.classList.add('un-checked');
+      // removes un-checked from all tabs
+      if(tab.previousElementSibling.classList.contains('un-checked')) {
+        tab.previousElementSibling.classList.remove('un-checked');
+        tab.previousElementSibling.removeAttribute('checked');
+      }
+    })
+  };
+
+  // sets checked attribute to false
+  // adds class to hide pui-tab-panel with css
+  let uncheck = (tab) => {
+    tab.previousElementSibling.setAttribute('checked', false);
+    tab.previousElementSibling.classList.add('un-checked');
+    console.log('add un-checked');
+  }
+
+  let onTabClicked = (evt) => {
+    if(evt.target.closest('.pui-tabs, .tabs').classList.contains('unresponsive')) {
+      console.log('unresponsive');
+      // do not run if flat tabs has unresponsive class
+      return;
+    } else {
+      // call mobileTabs function
+      mobileTabs(evt.target);
     }
+  };
 
   let fireMobileTabs = () => {
     tabs.forEach(tab => {
-      tab.addEventListener('click', () => {
-        // checks for flat tabs look
-        if(tab.closest('.pui-tabs, .tabs').classList.contains('pui-tabs--flat', 'tabs--flat')) {
-  
-          // checks for responsive class added to flat tabs
-          if(tab.closest('.pui-tabs, .tabs').classList.contains('responsive')) {
-            mobileTabs(tab);
-          } else {
-            return;
-          }
-        } else {
-          mobileTabs(tab);
+      tab.addEventListener('click', onTabClicked);
+    })
+  }
+
+  let checkRunTabs = () => {
+    let resizerHandled = false;
+    tabs.forEach(tab => {
+      tab.removeEventListener('click', onTabClicked);
+    })
+    pageWidth = window.innerWidth || document.documentElement.clientWidth;
+    if (pageWidth > 768) {
+      console.log('Wide viewport');
+      tabs.forEach(tab => {
+        if (tab.classList.contains('un-checked')) {
+          tab.classList.remove('un-checked');
         }
       })
-    })
-  }  
-
-  // if screen is under 768, run mobile tabs JS
-  if (pageWidth < 768) {
-    fireMobileTabs();
+      resizerHandled = false;
+      console.log(resizerHandled);
+      return;
+    } else {
+      if (resizerHandled === false ) {
+        fireMobileTabs();
+        resizerHandled = true;
+        console.log(resizerHandled);
+      }
+    }
   }
-  
+
+  window.addEventListener('resize', checkRunTabs, false);
+
+  let pageWidth = window.innerWidth || document.documentElement.clientWidth;
+  if (pageWidth < 768) {
+    checkRunTabs();
+  }
 }
