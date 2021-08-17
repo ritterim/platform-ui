518359423cef88daa348a83a3f2d0388f3db15bf
tedk13 <ted.krueger@ritterim.com>
2021-08-17 14:28:14 -0400

    js refactor

--- src/assets/js/src/carousel.js
+++ src/assets/js/src/carousel.js
@@ -1,11 +1,11 @@
-let carousel = document.querySelectorAll('.carousel');
+let carousels = document.querySelectorAll('.carousel');
 
-if(carousel) {
-  carousel.forEach(carousel => {
-    let slide = carousel.querySelectorAll('.carousel__slide');
-    let slideLength = slide.length
+if(carousels) {
+  carousels.forEach(carousel => {
+    let slides = carousel.querySelectorAll('.carousel__slide');
+    let slideLength = slides.length
     let slideArr = [];
-    let max = true;
+    let next = true;
     let slideVal = 0
     let increment = 1;
   
@@ -17,28 +17,28 @@ if(carousel) {
     
     // populate data attributes for slides
     let setSlideData = () => {
-      let counter = 0
-      slide.forEach(item => {
+      let counter = 0;
+      slides.forEach(slide => {
         // add data attribute (data-slide="1") for slide target
-        item.setAttribute('data-slide', counter);
+        slide.setAttribute('data-slide', counter);
     
         counter = counter + 1;
       })
     }
   
     let changeSlideVal = () => {
-      if (max == true && slideVal <= slideLength) {
-        slideVal += increment
+      if (next == true && slideVal <= slideLength) {
+        slideVal += increment;
   
         if (slideVal == slideLength) {
-          slideVal = 0
+          slideVal = 0;
         }
       } else {
-        max = false
+        next = false;
         slideVal -= increment;
   
         if (slideVal == -1) {
-          slideVal = slideLength -1
+          slideVal = slideLength -1;
         }
       }
     }
@@ -58,18 +58,18 @@ if(carousel) {
   
     // click an arrow to move the carousel one space
     let clickArrow = () => {
-      let arrow = carousel.querySelectorAll('.carousel__arrow');
-      arrow.forEach(arrow => {
+      let arrows = carousel.querySelectorAll('.carousel__arrow');
+      arrows.forEach(arrow => {
         arrow.addEventListener('click', e => {
           removeActiveSlide();
   
           // Which arrow is clicked
           if(arrow.classList.contains('carousel__arrow--next')) {
             // Go forward
-            max = true;
+            next = true;
           } else {
             // Go backward
-            max = false;
+            next = false;
           }
   
           // changes slideVal
@@ -92,7 +92,7 @@ if(carousel) {
     // Remove active class from current active button in nav
     let removeActiveButton = () => {
       if(carousel.querySelector('.carousel__nav-button.active') !== null) {
-        carousel.querySelector('.carousel__nav-button.active').classList.remove('active')
+        carousel.querySelector('.carousel__nav-button.active').classList.remove('active');
       }
     }
   
@@ -104,48 +104,19 @@ if(carousel) {
     // build the nav list
     let buildNavList = () => {
       let list = document.createElement('ol');
-      slide.forEach(() => {
-        let li = document.createElement('li')
+      list.classList.add('carousel__nav', 'list');
+      slides.forEach((slide, index) => {
+        let li = document.createElement('li');
+        li.classList.add('carousel__nav-item');
         let button = document.createElement('button');
+        button.classList.add('carousel__nav-button');
+        button.setAttribute('data-nav', index);
         li.appendChild(button);
         list.appendChild(li);
       });
   
       // append the nav list
       carousel.appendChild(list);
-  
-      // apply classes to list and list elements
-      listClasses();
-      liClasses();
-      buttonClasses();
-    }
-  
-    // add classes to list
-    let listClasses = () => {
-      let nav = carousel.querySelector('.carousel ol');
-      let navClasses = ['carousel__nav', 'list'];
-      nav.classList.add(...navClasses);
-    }
-  
-    let liClasses = () => {
-      // add class to nav li
-      let navItem = carousel.querySelectorAll('.carousel__nav li')
-      navItem.forEach(li => {
-        li.classList.add('carousel__nav-item');
-      });
-    }
-  
-    // add class and data attribute to nav button
-    let navButton;
-    let buttonClasses = () => {
-      navButton = carousel.querySelectorAll('.carousel__nav-item button');
-      let num = 0
-      navButton.forEach(btn => {
-        btn.classList.add('carousel__nav-button');
-        btn.setAttribute('data-nav', num);
-  
-        num = num + 1;
-      });
     }
   
     // highlight first button in nav by adding active class
@@ -156,14 +127,15 @@ if(carousel) {
   
     // click nav buttons
     let navButtonClick = () => {
-      navButton.forEach(btn => {
+      let navButtons = carousel.querySelectorAll('.carousel__nav-item button');
+      navButtons.forEach(btn => {
         btn.addEventListener('click', (e) => {
           let targetButton = e.target;
           
           removeActiveButton();
   
           // add 'active' to the clicked button
-          targetButton.classList.add('active')
+          targetButton.classList.add('active');
           
           // set slideVal to data attr from clicked nav button
           slideVal = parseInt(targetButton.dataset.nav);
