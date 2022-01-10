53dec0b8e7e3bac70802270cf7c9bc89bcb93f1b
Eric Ridolfi <eric.ridolfi@rimdev0131m.local>
2022-01-10 08:11:39 -0500

    Refactor to add active class to accordion

--- src/assets/js/src/accordion.js
+++ src/assets/js/src/accordion.js
@@ -2,14 +2,15 @@ let accordionHeaders = document.querySelectorAll('.pui-accordion__header, .accor
 
 if(accordionHeaders) {
   accordionHeaders.forEach(accordion => {
+    console.warn(accordion.parentElement);
     accordion.addEventListener('click', e => {
       e.preventDefault();
-      accordion.parentElement.querySelector('.pui-accordion__content, .accordion__content').classList.toggle('active');
+      accordion.parentElement.classList.toggle('active');
     });
     accordion.addEventListener('keydown', e => {
       if (e.keyCode === 13) {
         e.preventDefault();
-        accordion.parentElement.querySelector('.pui-accordion__content, .accordion__content').classList.toggle('active');
+        accordion.parentElement.classList.toggle('active');
       }
     });
   });
