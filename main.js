b34909371d1b2c393bd3beadd8a001165c6b925b
Ryan Trimble <mrtrimble2@gmail.com>
2022-10-27 15:15:51 -0400

    convert platform ui to vite

---
+++ main.js
@@ -0,0 +1,16 @@
+// Platform UI Styles
+import './src/sass/main.scss';
+
+// Platform UI JavaScript Components
+import './src/js/accordion';
+import './src/js/aside-toggle';
+import './src/js/background-image';
+import './src/js/drawer';
+import './src/js/forms';
+import './src/js/gradient';
+import './src/js/info-box';
+import './src/js/menu';
+import './src/js/mobile-menu';
+import './src/js/modal';
+import './src/js/tabs';
+import './src/js/z-index';
