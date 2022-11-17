9b311ad8601a91b300c059572097f4b4606aad29
Ted Krueger <ted.krueger@ritterim.com>
2022-11-17 15:42:27 -0500

    add a11y-color-tokens

---
+++ src/js/colors.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    name: "primary",
+    color: "#003f70"
+  },
+  {
+    name: "secondary",
+    color: "#91b9dc"
+  },
+  {
+    name: "button-primary",
+    color: "#3f7bad"
+  },
+  {
+    name: "button-secondary",
+    color: "#e58967"
+  },
+  {
+    name: "message-success",
+    color: "green"
+  },
+  {
+    name: "message-error",
+    color: "tomato"
+  },
+  {
+    name: "message-warning",
+    color: "#866657"
+  },
+  {
+    name: "message-info",
+    color: "#3f7bad"
+  },
+  {
+    name: "text",
+    color: "#333333"
+  },
+  {
+    name: "link",
+    color: "dodgerblue"
+  }
+];
