8a400fe6f4bb758c30905cd30c3103b632b85eff
hougasian <kevinhougasian@kevins-MacBook-Pro.local>
2023-02-13 16:01:15 -0500

    remove 'after' tasks, use encrypted myget token not stored in appveyor

--- after_build.ps1
+++
@@ -1,9 +0,0 @@
-$gitHashPath = "./styleguide/_git_hash/index.html"
-
-New-Item $gitHashPath -Force
-Set-Content $gitHashPath $env:APPVEYOR_REPO_COMMIT
-
-$gitHashPath = "./styleguide/_health/index.html"
-
-New-Item $gitHashPath -Force
-Set-Content $gitHashPath "Healthy"
